6954fdaf3618045a651d0a6091bedbbfceabe034
xingchengxu <xingcheng.xu18@gmail.com>
2026-05-26 15:32:59 +0800

    scaffold: @openexpertise/authoring package

---
+++ packages/authoring/src/index.ts
@@ -0,0 +1,14 @@
+export { UltraExpertise, type UltraExpertiseOpts, type UltraResult } from './ultra.js'
+export {
+  writeDraft,
+  type WriteDraftOpts,
+  type WriteDraftResult,
+  PathTraversalError,
+} from './writer.js'
+export { slugify } from './slug.js'
+export {
+  ANALYSIS_SCHEMA,
+  SYNTHESIS_SCHEMA,
+  type AnalysisOutput,
+  type SynthesisOutput,
+} from './schemas.js'
